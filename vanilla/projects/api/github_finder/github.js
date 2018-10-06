de21b30f3c0fce60ec25277aa0a62d2c50d8e8c9
Steve Kim <suykim21@Steves-MacBook.local>
2018-10-05 22:25:47 -0700

    added regex form

--- vanilla/projects/api/github_finder/github.js
+++ vanilla/projects/api/github_finder/github.js
@@ -2,13 +2,21 @@ class GitHub {
   constructor() {
     this.client_id = 'dfd6b24045ebfbed9637';
     this.client_secret = 'a8f3ca15aafea3315e31178d883db62fbb1ab2fc';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repose_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
     const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
     return {
-      profile
+      profile,
+      repos
     }
   }
 }
