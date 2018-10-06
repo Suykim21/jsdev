5494547d25db00dfc0705d3af4e63344f6474452
Steve Kim <suykim21@Steves-MacBook.local>
2018-10-05 20:40:48 -0700

    added UI and github classes

--- vanilla/projects/api/github_finder/github.js
+++ vanilla/projects/api/github_finder/github.js
@@ -3,4 +3,12 @@ class GitHub {
     this.client_id = 'dfd6b24045ebfbed9637';
     this.client_secret = 'a8f3ca15aafea3315e31178d883db62fbb1ab2fc';
   }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+    const profile = await profileResponse.json();
+    return {
+      profile
+    }
+  }
 }
