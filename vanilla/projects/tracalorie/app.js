9322ce38ca38f2a0cf7f101c3912115081fc56bf
Steve Kim <suykim21@Steves-MacBook.local>
2018-10-10 21:39:15 -0700

    completed update function

--- vanilla/projects/tracalorie/app.js
+++ vanilla/projects/tracalorie/app.js
@@ -51,9 +51,35 @@ const ItemCtrl = (() => {
       // Loop through items
       data.items.forEach(item => {
         if(item.id === id) {
-          return found = item;
+          found = item;
         }
       });
+      return found;
+    },
+
+    updateItem: (name, calories) => {
+      // Calories to number
+      calories = parseInt(calories);
+
+      let found = null;
+
+      data.items.forEach(item => {
+        if(item.id === data.currentItem.id){
+          item.name = name;
+          item.calories = calories;
+          found = item;
+        }
+      });
+
+      return found;
+    },
+
+    setCurrentItem: item => {
+      data.currentItem = item;
+    },
+
+    getCurrentItem: () => {
+      return data.currentItem;
     },
 
     getTotalCalories: () => {
@@ -78,6 +104,7 @@ const ItemCtrl = (() => {
 const UICtrl = (() => {
   const UISelectors = {
     itemList: '#item-list',
+    listItems: '#item-list li',
     addBtn: '.add-btn',
     updateBtn: '.update-btn',
     deleteBtn: '.delete-btn',
@@ -132,9 +159,35 @@ const UICtrl = (() => {
       document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li);
     },
 
+    updateListItem: item => {
+      let listItems = document.querySelectorAll(UISelectors.listItems);
+
+      // Turn Node list into array
+      listItems = Array.from(listItems);
+
+      listItems.forEach(listItem => {
+        const itemID = listItem.getAttribute('id');
+
+        if(itemID ===  `item-${item.id}`) {
+          document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>`;
+        }
+      });
+    },
+
     clearInput: () => {
       document.querySelector(UISelectors.itemNameInput).value = '';
       document.querySelector(UISelectors.itemCaloriesInput).value = '';
+    },
+
+    addItemToForm: () => {
+      document.querySelector(UISelectors.itemNameInput).value = 
+      ItemCtrl.getCurrentItem().name;
+      document.querySelector(UISelectors.itemCaloriesInput).value = 
+      ItemCtrl.getCurrentItem().calories;
+      UICtrl.showEditState();
     },
 
     hideList: () => {
@@ -153,6 +206,13 @@ const UICtrl = (() => {
       document.querySelector(UISelectors.addBtn).style.display = 'inline';
     },
 
+    showEditState: () => {
+      document.querySelector(UISelectors.updateBtn).style.display = 'inline';
+      document.querySelector(UISelectors.deleteBtn).style.display = 'inline';
+      document.querySelector(UISelectors.backBtn).style.display = 'inline';
+      document.querySelector(UISelectors.addBtn).style.display = 'none';
+    },
+
     getSelectors: () => {
       return UISelectors;
     }
@@ -169,8 +229,19 @@ const AppCtrl = ((ItemCtrl, UICtrl) => {
     // Add item event
     document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
+    // Disable submit on enter
+    document.addEventListener('keypress', e => {
+      if(e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+      }
+    });
+
     // Edit icon click event
-    document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+    document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+
+    // Update item event
+    document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
   }
 
   // Add item submit
@@ -199,8 +270,8 @@ const AppCtrl = ((ItemCtrl, UICtrl) => {
     e.preventDefault();
   }
 
-  // Update item subkit
-  const itemUpdateSubmit = e => {
+  // Click edit item
+  const itemEditClick = e => {
 
     if(e.target.classList.contains('edit-item')) {
       // Get list item id (item-0, item-1)
@@ -215,8 +286,34 @@ const AppCtrl = ((ItemCtrl, UICtrl) => {
       // Get item
       const itemToEdit = ItemCtrl.getItemById(id);
 
-      console.log(itemToEdit);
+      // Set current item
+      ItemCtrl.setCurrentItem(itemToEdit);
+
+      // Add item to form
+      UICtrl.addItemToForm();
     }
+    e.preventDefault();
+  }
+
+  // Update item submit
+  const itemUpdateSubmit = e => {
+    // Get item input
+    const input = UICtrl.getItemInput();
+
+    // Update item
+    const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
+
+    // Update UI
+    UICtrl.updateListItem(updatedItem);
+
+    // Get total calories
+    const totalCalories = ItemCtrl.getTotalCalories();
+
+    // Add total calories to UI
+    UICtrl.showTotalCalories(totalCalories);
+
+    UICtrl.clearEditState();
+    
     e.preventDefault();
   }
 
