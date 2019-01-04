0d6a133d4d1136616feecc38afb2a15b40d563dc
Steve Kim <suykim21@Steves-MacBook.local>
2019-01-03 21:18:17 -0800

    refreshing on express router

--- nodejs/express/app.js
+++ nodejs/express/app.js
@@ -1,19 +1,18 @@
-const http = require('http');
-
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('In the middleware!');
-    next(); // Allows the request to continue to the next middleware in line
+const adminRoutes = require('./routes/admin'); // requires module.exports = router
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({extended: false})); //exposes the resulting object (key/values) on req.body - Extended: false does not allow nested objects
+
+app.use(adminRoutes); // uses routes from routes/admin.js
+app.use(shopRoutes);
+
+// 404 error - using use method to capture every request
+app.use((req, res, next) => { 
+    res.status(404).send('<h1>Page not found</h1>')
 });
 
-app.use((req, res, next) => {
-    console.log('In another middleware!');
-    res.send('<h1>Hello from Express!</h1>');
-});
-
-const server = http.createServer(app);
-
-server.listen(3000);
+app.listen(3000);
