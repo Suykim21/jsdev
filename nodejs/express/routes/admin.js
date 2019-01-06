9c14653db101eea960e37424d996fc9266916382
Steve Kim <suykim21@Steves-MacBook.local>
2019-01-05 16:19:29 -0800

    completed express fundamentals

--- nodejs/express/routes/admin.js
+++ nodejs/express/routes/admin.js
@@ -1,14 +1,18 @@
+const path = require('path');
 const express = require('express');
-
+// rootDir - /Users/suykim21/Desktop/dev/js/nodejs/express
+const rootDir = require('../util/path'); 
 const router = express.Router();
 
+// Filtering path /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  console.log('In another middleware!');
-  res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+  console.log(rootDir);
+  console.log('hi im here')
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-// Accepts only POST request
-router.post('/product', (req, res) => {
+// Accepts only POST request /admin/add-product => POST
+router.post('/add-product', (req, res) => {
   console.log(req.body);
   res.redirect('/');
 });
--- nodejs/express/routes/shop.js
+++ nodejs/express/routes/shop.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
-
+const rootDir = require('../util/path');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  // __dirname points to routes - ex: /Users/suykim21/Desktop/dev/js/nodejs/express/routes
+  res.sendFile(path.join(rootDir, 'views', 'shop.html')); // from routes go to express project folder and go to views and access shop.html
 });
 
 module.exports = router;
