627e95142f6fabe75a141ec3fa4bd7fda71bd557
Steve Kim <suykim21@Steves-MacBook.local>
2019-01-15 22:26:38 -0800

    added cart functionality with mongodb

--- nodejs/nodejsNoSQL/models/user.js
+++ nodejs/nodejsNoSQL/models/user.js
@@ -1,16 +1,80 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
+const ObjectId = mongodb.ObjectId;
 
-const sequelize = require('../util/database');
+class User {
+  constructor(username, email, cart, id) {
+    this.name = username;
+    this.email = email;
+    this.cart = cart; // {items: []}
+    this._id = id; 
+  }
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: Sequelize.STRING,
-  email: Sequelize.STRING
-});
+  save() {
+    const db = getDb;
+    return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex(cp => {
+      return cp.productId.toString() === product._id.toString();
+    });
+
+    let newQuantity = 1;
+    const updatedCartItems = [...this.cart.items] // new array - copy not ref
+
+    if (cartProductIndex >= 0) { // if there are items inside the cart - increment
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity
+    } else { // new item being added;
+      updatedCartItems.push({
+        productId: new ObjectId(product._id), 
+        quantity: newQuantity
+      });
+    }
+
+    const updatedCart = {
+      items: updatedCartItems
+    };
+
+    const db = getDb();
+    return db
+      .collection('users')
+      .updateOne(
+        {_id: new ObjectId(this._id)},
+        { $set: {cart: updatedCart}}
+      );
+  }
+
+  getCart() {
+    const db = getDb();
+    const productIds = this.cart.items.map(i => {
+      console.log('map', i.productId);
+      return i.productId;
+    });
+    return db.collection('products')
+      .find({_id: {$in: productIds}})
+      .toArray()
+      .then(products => {
+        return products.map(p => {
+          return {...p, quantity: this.cart.items.find(i => {
+              return i.productId.toString() === p._id.toString();
+          }).quantity
+        }
+      });
+    });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users')
+      .findOne({_id: new ObjectId(userId)})
+      .then(user => {
+        console.log(user)
+        return user;
+      })
+      .catch(err => console.log(err));
+  }
+}
 
 module.exports = User;
